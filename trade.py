c0d09b593c2f4b54eb6763d088d6860aa1c2fdde
Ariel Cohen <ariel042cohen@gmail.com>
2017-12-04 22:34:17 +0200

    Add simulator

--- trade.py
+++ trade.py
@@ -5,16 +5,18 @@ import time
 ratioList = []
 
 def main():
- averageInterval = 0
  index = 0
- allaverage = 0
- changeRatioAll = 0
+ bittrexAvg = 11126.0 * 3.49
+ bit2cAvg = 40700.60782700422
 
  while True:
-     bit2CTicker = getBit2CTicker()
-     bittrexTicker = getBittrexTicker()
-     bit2cAvg = bit2CTicker["av"]
-     bittrexAvg = float(bittrexTicker["mid"])
+     # bit2CTicker = getBit2CTicker()
+     # bittrexTicker = getBittrexTicker()
+     # bit2cAvg = bit2CTicker["av"]
+     # bittrexAvg = float(bittrexTicker["mid"])
+
+     bit2cAvg = getBit2CAverage(bit2cAvg)
+     bittrexAvg = getBitterexAverage(bittrexAvg)
 
      ratio = bit2cAvg / bittrexAvg
      if index == 0:
@@ -23,15 +25,16 @@ def main():
          continue
 
      currentRatioAverege = getRatioAverege()
-     if ((currentRatioAverege - ratio) * 100 > 0.1) :
-         print((currentRatioAverege - ratio) * 100)
-
+     print('bit2cAvg: ' + str(bit2cAvg))
+     print('bittrexAvg: ' + str(bittrexAvg))
+     print('ratio: ' + str((ratio - currentRatioAverege) / ratio * 100))
+     print('\n\n')
      addNewRatio(ratio)
      time.sleep(3)
 
 
 def addNewRatio(ratio):
-    if (len(ratioList) == 30):
+    if (len(ratioList) == 2):
         del ratioList[0]
         ratioList.append(ratio)
     else:
@@ -40,22 +43,17 @@ def addNewRatio(ratio):
 def getRatioAverege():
     return sum(ratioList) / len(ratioList)
 
-def percentBreakEven(ticker):
-    comssion = 0.5
-    space = 0.5
-    bid = ticker["h"]
-    ask = ticker["l"]
-    breakEven = bid / ask
-
 def getBit2CTicker():
     req = requests.get("https://www.bit2c.co.il/Exchanges/BtcNis/Ticker.json")
     ticker = json.loads(req.content)
     return ticker
 
-def getBittrexTicker():
-    req = requests.get("https://api.bitfinex.com/v1/pubticker/btcusd")
-    ticker = json.loads(req.content)
-    return ticker
+def getBitterexAverage(average):
+    average = average + 1
+    return average
+
+def getBit2CAverage(average):
+    return average
 
 if __name__ == '__main__':
     main()
