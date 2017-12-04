fec463ab2a72d42334038693bf82c7750d018924
Ariel Cohen <ariel042cohen@gmail.com>
2017-12-04 21:11:17 +0200

    Start trade.py

--- trade.py
+++ trade.py
@@ -1,24 +1,44 @@
 import requests
 import json
+import time
+
+ratioList = []
 
 def main():
- absuluteRatio = 1.02
- bit2CTicker = getBit2CTicker()
- bittrexTicker = getBittrexTicker()
+ averageInterval = 0
+ index = 0
+ allaverage = 0
+ changeRatioAll = 0
 
  while True:
+     bit2CTicker = getBit2CTicker()
+     bittrexTicker = getBittrexTicker()
      bit2cAvg = bit2CTicker["av"]
-     bittrexAvg = bittrexTicker["mid"]
-     changeRatio = bit2cAvg / bittrexAvg
-     breakEven = percentBreakEven(bit2CTicker)
+     bittrexAvg = float(bittrexTicker["mid"])
 
-     if changePrecent > breakEven {
+     ratio = bit2cAvg / bittrexAvg
+     if index == 0:
+         index = index + 1
+         addNewRatio(ratio)
+         continue
 
-     }
+     currentRatioAverege = getRatioAverege()
+     if ((currentRatioAverege - ratio) * 100 > 0.1) :
+         print((currentRatioAverege - ratio) * 100)
+
+     addNewRatio(ratio)
+     time.sleep(3)
 
 
+def addNewRatio(ratio):
+    if (len(ratioList) == 30):
+        del ratioList[0]
+        ratioList.append(ratio)
+    else:
+        ratioList.append(ratio)
 
-
+def getRatioAverege():
+    return sum(ratioList) / len(ratioList)
 
 def percentBreakEven(ticker):
     comssion = 0.5
@@ -30,10 +50,12 @@ def percentBreakEven(ticker):
 def getBit2CTicker():
     req = requests.get("https://www.bit2c.co.il/Exchanges/BtcNis/Ticker.json")
     ticker = json.loads(req.content)
+    return ticker
 
 def getBittrexTicker():
-    req = requests.get("https://api.bitfinex.com/v1/pubticker/dshusd")
+    req = requests.get("https://api.bitfinex.com/v1/pubticker/btcusd")
     ticker = json.loads(req.content)
+    return ticker
 
 if __name__ == '__main__':
     main()
